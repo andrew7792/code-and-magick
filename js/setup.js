ad2463d118337655ae97bef688d30dfa711bcd9e
andrew7792 <biorostukraine@gmail.com>
2018-08-05 00:43:27 +0300

    1 level

--- js/setup.js
+++ js/setup.js
@@ -1,55 +1,47 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-
-console.log(1);
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-var arrObjects = [ // объявление массива
+var heroes = [
+    {
+        name: "Иван Ирвинг",
+        coatColor: 'rgb(101, 137, 164)',
+        eyesColor: 'black'
+    },
 
-{
-    name: "Иван Ирвинг",
-    coatColor: 'rgb(101, 137, 164)',
-    eyesColor: 'black'
-},
+    {
+        name: "Хуан Себастьян Верон",
+        coatColor: 'rgb(241, 43, 107)',
+        eyesColor: 'red'
+    },
 
-{
-    name: "Хуан Себастьян Верон",
-    coatColor: 'rgb(241, 43, 107)',
-    eyesColor: 'red'
-},
+    {
+        name: "Мария Мирабелла",
+        coatColor: 'rgb(146, 100, 161)',
+        eyesColor: 'blue'
+    },
 
-{
-    name: "Мария Мирабелла",
-    coatColor: 'rgb(146, 100, 161)',
-    eyesColor: 'blue'
-},
-
-{
-    name: "Кристоф Вальц",
-    coatColor: 'rgb(56, 159, 117)',
-    eyesColor: 'yellow'
-}
+    {
+        name: "Кристоф Вальц",
+        coatColor: 'rgb(56, 159, 117)',
+        eyesColor: 'yellow'
+    }
 ];
 
-var randomTemplate = document.querySelector('#similar-wizard-template');
-for(var i =0; i < arrObjects.length; i++ ){
+for(var i = 0; i < heroes.length; i++ ){
+    var wizzardElement = similarWizardTemplate.cloneNode(true);
 
-
-    for(var j = 0; j < arrObjects.length; j++){
-
-
-    }
-
+    var heroName = wizzardElement.querySelector('.setup-similar-label');
+    heroName.textContent = heroes[i].name//имя игрока
+    var wizzardCoat = wizzardElement.querySelector('.wizard-coat');
+    wizzardCoat.style.fill = heroes[i].coatColor//Цвет мантии coatColor задайте как цвет заливки fill в стилях элемента
+    var wizzardEyes = wizzardElement.querySelector('.wizard-eyes');
+    wizzardEyes.style.fill = heroes[i].eyesColor//Цвет глаз eyesColor задайте как цвет заливки fill в стилях элемента eyesColor
+    similarListElement.appendChild(wizzardElement);
 }
 
 
-
-var nameHero = function(arrObjects){
-    document.querySelector('.setup-similar-label');
-    //.appendChild(arrObjects[i].name)
-
-}
